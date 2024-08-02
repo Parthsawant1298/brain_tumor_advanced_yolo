2b680faafb5ef26faf522151546c8dfb879b26ee
Parthsawant1298 <145202363+Parthsawant1298@users.noreply.github.com>
2024-08-02 17:49:54 +0530

    Update final.py

--- final.py
+++ final.py
@@ -7,9 +7,9 @@ import cv2
 import os
 
 # Constants
-MODEL_PATH_CNN = r'C:\Users\parth sawant\Desktop\New folder\mri.keras'
-MODEL_PATH_YOLO = r'C:\Users\parth sawant\Desktop\New folder\best_model (4).pt'
-OUTPUT_IMAGE_PATH = r'C:\Users\parth sawant\Desktop\New folder\annotated_image.jpg'
+MODEL_PATH_CNN = "C:\Users\parth sawant\Desktop\New folder\mri.keras"
+MODEL_PATH_YOLO = "C:\Users\parth sawant\Desktop\New folder\best_model (4).pt"
+OUTPUT_IMAGE_PATH = "C:\Users\parth sawant\Desktop\New folder\annotated_image.jpg"
 CONFIDENCE_THRESHOLD = 0.5  # Confidence threshold for YOLO predictions
 
 # Define class labels for CNN
